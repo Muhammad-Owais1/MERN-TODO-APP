5473cd63230570e75ec2f2557317f128527b04c8
MuhammadOwais <141480957+Muhammad-Owais1@users.noreply.github.com>
2024-07-10 15:19:37 +0500

    Minor fixes made.

--- client/src/components/UserButtons.jsx
+++ client/src/components/UserButtons.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function UserButtons() {
-
-    return (
-        <>
-            <h1 className='text-center pt-28 pb-10 font-["Montserrat"] font-semibold text-4xl text-yellow-400'>Login And Start Using</h1>
-            <div className='text-white flex flex-wrap justify-center items-center gap-10'>
-                <button className='h-10 w-24 border-2 rounded-3xl font-["Montserrat"] uppercase text-xs hover:text-yellow-400 hover:border-yellow-400 '><Link className='h-full w-full flex justify-center items-center' to='/user/login'>login</Link></button>
-                <button className='h-10 w-24 border-2 rounded-3xl font-["Montserrat"] uppercase text-xs hover:text-yellow-400 hover:border-yellow-400 '><Link className='h-full w-full flex justify-center items-center' to='/user/signup'>signup</Link></button>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <h1 className='text-center pt-28 pb-10 font-["Montserrat"] font-semibold text-4xl '>
+        Login And Start Using
+      </h1>
+      <div className=" flex flex-wrap justify-center items-center gap-10">
+        <button className='h-10 w-24 border-2 border-black rounded-3xl font-["Montserrat"] uppercase text-xs  '>
+          <Link
+            className="h-full w-full flex justify-center items-center"
+            to="/user/login"
+          >
+            login
+          </Link>
+        </button>
+        <button className='h-10 w-24 border-2 border-black rounded-3xl font-["Montserrat"] uppercase text-xs  '>
+          <Link
+            className="h-full w-full flex justify-center items-center"
+            to="/user/signup"
+          >
+            signup
+          </Link>
+        </button>
+      </div>
+    </>
+  );
 }
